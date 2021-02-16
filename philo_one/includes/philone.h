186adb432f3467ef5754cabc522532d7f432c26d
Theo Jacquelin <thjacque@z4r11p5.42lyon.fr>
2021-02-16 11:25:11 +0100

    multithread sleep test

--- philo_one/includes/philone.h
+++ philo_one/includes/philone.h
@@ -6,7 +6,7 @@
 /*   By: thjacque <thjacque@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 14:00:45 by thjacque          #+#    #+#             */
-/*   Updated: 2021/02/15 15:20:00 by thjacque         ###   ########lyon.fr   */
+/*   Updated: 2021/02/16 11:05:29 by thjacque         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,25 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+typedef struct		s_philos
+{
+	int				id;
+	int				tts;
+	pthread_t		thread;
+	struct s_params *p;
+}					t_philos;
+
+
 typedef struct		s_params
 {
 	struct timeval	start;
-	int				phils;
-	int				forks;
+	int				nphils;
 	int				ttd;
 	int				tte;
 	int				tts;
 	int				noe;
+	pthread_mutex_t	**fork;
+	t_philos		*philo;
 }					t_params;
 int			ft_atoi(const char *nbtr);
 int			print_error(char *s, t_params *p);
--- philo_one/srcs/main.c
+++ philo_one/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: thjacque <thjacque@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 13:59:56 by thjacque          #+#    #+#             */
-/*   Updated: 2021/02/15 15:27:08 by thjacque         ###   ########lyon.fr   */
+/*   Updated: 2021/02/16 11:17:57 by thjacque         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,11 @@ t_params	*init_params(int ac, char **av)
 	gettimeofday(&p->start, NULL);
 	if (ac < 5 || ac > 6)
 		return (p);
-	p->phils = ft_atoi(av[1]);
-	p->forks = ft_atoi(av[1]);
+	p->nphils = ft_atoi(av[1]);
+	if (!(p->philo = malloc(p->nphils * sizeof(t_philos))))
+		return (NULL);
+	if (!(p->fork = malloc(p->nphils * sizeof(pthread_mutex_t *))))
+		return (NULL);
 	p->ttd = ft_atoi(av[2]);
 	p->tte = ft_atoi(av[3]);
 	p->tts = ft_atoi(av[4]);
@@ -36,7 +39,7 @@ t_params	*init_params(int ac, char **av)
 void	print_params(t_params *p)
 {
 	prefix(p);
-	printf("\033[34mN of Philo and Forks: \033[92m%d\033[0m\n", p->phils);
+	printf("\033[34mN of Philo and Forks: \033[92m%d\033[0m\n", p->nphils);
 	prefix(p);
 	printf("\033[34mTime to die: \033[92m%d\033[0m\n", p->ttd);
 	prefix(p);
@@ -47,6 +50,40 @@ void	print_params(t_params *p)
 	printf("\033[34mNumber of eating max: \033[92m%d\033[0m\n", p->noe);
 }
 
+void	*simu(void *philo)
+{
+	t_philos	*p;
+	
+	p = philo;
+	while (1)
+	{
+		usleep(p->tts * 1000 + (p->p->nphils - p->id * 1000000));
+		prefix(p->p);
+		printf("Philo %d is now sleeping zzZZ\n", p->id);
+		usleep(p->tts * 1000 + (p->p->nphils - p->id * 1000000));
+		prefix(p->p);
+		printf("Philo %d wake up !\n", p->id);
+	}
+	return (philo);
+}
+
+void	init_philos(t_params *p)
+{
+	int		i;
+
+	i = -1;
+	while (++i < p->nphils)
+	{
+		p->philo[i].id = i;
+		p->philo[i].tts = p->tts;
+		p->philo[i].p = p;
+		pthread_create(&p->philo[i].thread, NULL, simu, &p->philo[i]);
+	}
+	i = -1;
+	while (++i < p->nphils)
+		pthread_join(p->philo[i].thread, NULL);
+}
+
 int		main(int ac, char **av)
 {
 	t_params *p;
@@ -55,6 +92,7 @@ int		main(int ac, char **av)
 	if (ac < 5 || ac > 6)
 		return (print_error("Incorrect number of arguments !", p));
 	print_params(p);
+	init_philos(p);
 	free(p);
 	return (0);
 }
